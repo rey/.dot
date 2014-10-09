125096d3e4890f50f746a918e204eb91720d8887
Rey Dhuny <rey@users.noreply.github.com>
2014-10-09 13:45:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ cd ~
 ln -sf ~/.dot/bash_profile ~/.bash_profile
 ln -sf ~/.dot/bashrc ~/.bashrc
 ln -sf ~/.dot/gitconfig ~/.gitconfig
-ln -sf ~/.dot/gitconfig_global ~/.gitconfig_global
+ln -sf ~/.dot/gitignore_global ~/.gitconfig_global
 ln -sf ~/.dot/signature ~/.signature

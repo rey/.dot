cd4a94e8c08859b016b14dc8610f9bf9276f6864
Rey Dhuny <rey@me.com>
2019-02-05 11:49:36 +0000

    Remove plan and signature

--- install.sh
+++ install.sh
@@ -5,6 +5,4 @@ ln -sf ~/.dot/bash_profile ~/.bash_profile
 ln -sf ~/.dot/bashrc ~/.bashrc
 ln -sf ~/.dot/gitconfig ~/.gitconfig
 ln -sf ~/.dot/gitignore_global ~/.gitignore_global
-ln -sf ~/.dot/signature ~/.signature
-ln -sf ~/.dot/plan ~/.plan
 ln -sf ~/.dot/inputrc ~/.inputrc
